661c7a337101182b779f67bd877ccb07294807d5
draxyolo <47677998+draxyolo@users.noreply.github.com>
2025-12-27 14:33:00 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,9 @@
-page.tsx
+<<<<<<< HEAD
+import React from 'react';
+
+export default function Home() {
+=======
+export default function Page() {
+>>>>>>> 965129431a06346d05f21b4188abeb3a3b23980a
+  return <div>OK</div>;
+}
